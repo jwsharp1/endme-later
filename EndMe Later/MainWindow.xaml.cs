b96e76c55c85cd06a533127dc4c5e33c69cef42e
jwsharp1 <jwsharp1@asu.edu>
2017-02-19 23:54:47 -0700

    Volume Reducer Functionality
    NEW:
    - Added volume reducer
    `The system volume will reduce at specific intervals of the timer.`
    
    TODO: Major code cleanup

--- EndMe Later/MainWindow.xaml.cs
+++ EndMe Later/MainWindow.xaml.cs
@@ -4,35 +4,38 @@ namespace EndMe_Later
 {
     public partial class MainWindow
     {
-        public const int HOUR_IN_SECONDS = 3600;
-        SleepTimer st = new SleepTimer();
+        public const int HOUR_IN_SECONDS = 36;
+        Timer t = new Timer();
         bool sleeping;
 
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new SleepTimer();
-            st.main = this;
+            t.main = this;
         }
 
         // calculate sleep time in seconds and send to sdtimer
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            if (sleepCheckBox.IsChecked == true)
+            if (slider.Value != 0)
             {
-                int sleepValue = (int)(slider.Value * HOUR_IN_SECONDS);
-                st.makeSleepTimer(sleepValue);
-                sleeping = true;
+                int sliderValue = (int)(slider.Value * HOUR_IN_SECONDS);
+                t.makeTimer(sliderValue);
+
+                if (sleepCheckBox.IsChecked == true)
+                {
+                    sleeping = true;
+                }
             }
         }
 
         // button for canceling shutdown
         private void button1_Click(object sender, RoutedEventArgs e)
         {
+            t.stopTimer();
+            timeRemaining.Text = "Stopped";
             if (sleeping)
             {
-                st.stopSleepTimer();
-                timeRemaining.Text = "Stopped";
                 sleeping = false;
             }
         }
---
+++ EndMe Later/Volume.cs
@@ -0,0 +1,40 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Runtime.InteropServices;
+
+namespace EndMe_Later
+{
+    class Volume
+    {
+        public int GetVolume()
+        {
+            int result = 100;
+            try
+            {
+                MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
+                MMDevice device = DevEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+                result = (int)(device.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+            }
+            catch (Exception)
+            {
+            }
+
+            return result;
+        }
+
+        public void SetVolume(int value)
+        {
+            try
+            {
+                MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
+                MMDevice device = DevEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+                device.AudioEndpointVolume.MasterVolumeLevelScalar = (float)value / 100.0f;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+    }
+}
