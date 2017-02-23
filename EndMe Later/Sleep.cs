de7eb625a6903c68a30b338a8fc369cbf6eef9d3
jwsharp1 <jwsharp1@asu.edu>
2017-02-22 20:59:39 -0700

    Actual sleep functionality!
    CHANGED:
    -Program now sleeps instead of shutting down
    -Minor text fixes

--- EndMe Later/Sleep.cs
+++ EndMe Later/Sleep.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Windows;
-using System.Windows.Threading;
+//using System.Windows;
+using System.Windows.Forms;
 
 namespace EndMe_Later
 {
@@ -8,8 +8,9 @@ namespace EndMe_Later
     {
         public void sleep()
         {
+            Application.SetSuspendState(PowerState.Suspend, false, false);
             /*startInfo.FileName = "rundll32";
-            startInfo.Arguments = "powrprof.dll,SetSuspendState 0,1,0";*/
+            startInfo.Arguments = "powrprof.dll,SetSuspendState 0,1,0";
             System.Diagnostics.Process process = new System.Diagnostics.Process();
             System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
             startInfo.FileName = "shutdown";
@@ -17,7 +18,7 @@ namespace EndMe_Later
             startInfo.CreateNoWindow = true;
             startInfo.UseShellExecute = false;
             process.StartInfo = startInfo;
-            process.Start();
+            process.Start();*/
         }
     }
 }
